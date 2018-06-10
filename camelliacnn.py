d50aca6d3232959a6d758d4b7e70819f40ff1a3e
Shinelle Prescott <kitachan21@hotmail.com>
2018-06-10 14:16:51 +0900

    Add files via upload
    added code for graph visualizations

--- camelliacnn.py
+++ camelliacnn.py
@@ -4,6 +4,12 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.utils.vis_utils import plot_model
+import matplotlib.pyplot as plt 
+import os
+
+
+os.environ["PATH"] += os.pathsep + 'D:\Program Files (x86)\Anaconda\Graphviz2.38\bin'
 
 #Stage 1 - Preparing CNN
 
@@ -26,6 +32,7 @@ classifier.add(Flatten())
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
+plot_model(classifier, to_file='class_plot.png', show_shapes = True, show_layer_names=True)
 
 # Compilation of CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -63,11 +70,35 @@ test_dataset = test_datagen.flow_from_directory(
 
 #Comment out when attempting prediction
 
-classifier.fit_generator(training_dataset,
+history = classifier.fit_generator(training_dataset,
                         steps_per_epoch = 800,
                         epochs = 25,
-                       validation_data = test_dataset,
+                      validation_data = test_dataset,
                         validation_steps = 400)
+
+print(history.history.keys())
+plt.figure(1)
+
+ # summarize history for accuracy  
+   
+plt.subplot(211)  
+plt.plot(history.history['acc'])  
+plt.plot(history.history['val_acc'])  
+plt.title('model accuracy')  
+plt.ylabel('accuracy')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+   
+ # summarize history for loss  
+   
+plt.subplot(212)  
+plt.plot(history.history['loss'])  
+plt.plot(history.history['val_loss'])  
+plt.title('model loss')  
+plt.ylabel('loss')  
+plt.xlabel('epoch')  
+plt.legend(['train', 'test'], loc='upper left')  
+plt.show()  
 
 
 #Stage 3 - Make new prediction
